3afb4646114b5888ce7c65ad82c2270ec634b14e
MelissaAyllon <mayllon@espol.edu.ec>
2025-03-15 13:15:00 -0500

    UPDATE user type to a diff file

--- aspy-web/src/components/ProfileView.tsx
+++ aspy-web/src/components/ProfileView.tsx
@@ -1,38 +1,30 @@
 import penToSquare from '../assets/pen-to-square.svg';
+import { User } from '../types/User';
 
-interface UserInfo {
-    name: string;
-    rol: string;
-    image: string;
-    aboutme: string;
-    age: number;
-    gender: string;
-}
-
-function ProfileView(user_info: UserInfo) { 
+function ProfileView(user_info: User) { 
     return (
         <div className='flex flex-row md:flex-wrap justify-center gap-16 p-8 m-8'>
             <div className='flex flex-col gap-16 items-center'>
                 <img className='rounded-full w-auto h-auto' src={user_info.image} alt={user_info.name} />
                 <div className='flex flex-col gap-1 justify-center items-center'>
-                    <h1 className='text-base'>{user_info.name}</h1>
-                    <h2 className='text-sm text-gray-400'>{user_info.rol}</h2>
+                    <h1 className='font-kumbh text-primaryAspy font-semibold text-base'>{user_info.name}</h1>
+                    <h2 className='font-kumbh text-secondaryAspy text-sm'>{user_info.rol}</h2>
                 </div>
                 <img src={penToSquare} className= 'fill-gray-200 w-1/6 h-1/6 ' alt="Editar perfil" />
             </div>
             <div className='flex flex-col gap-16'>
                 <div className='flex flex-col gap-2'>
-                    <h1 className='text-base'>Sobre mí</h1>
-                    <p className='text-sm text-gray-400'>{user_info.aboutme}</p>
+                    <h1 className='font-kumbh text-primaryAspy font-semibold text-base'>Sobre mí</h1>
+                    <p className='font-kumbh text-sm text-secondaryAspy'>{user_info.aboutme}</p>
                 </div>
                 <div className='flex flex-row gap-16'>
                     <div className='flex flex-col gap-2'>
-                        <h2 className='text-base'>Edad</h2>
-                        <p className='text-sm text-gray-400'>{user_info.age}</p>
+                        <h2 className='font-kumbh text-primaryAspy font-semibold text-base'>Edad</h2>
+                        <p className='font-kumbh text-sm text-secondaryAspy'>{user_info.age}</p>
                     </div>
                     <div className='flex flex-col gap-2'>
-                        <h2 className='text-base'>Género</h2>
-                        <p className='text-sm text-gray-400'>{user_info.gender}</p>
+                        <h2 className='font-kumbh text-primaryAspy font-semibold text-base'>Género</h2>
+                        <p className='font-kumbh text-sm text-secondaryAspy'>{user_info.gender}</p>
                     </div>
                 </div>
             </div>
---
+++ aspy-web/src/types/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+    name: string;
+    rol: string;
+    image: string;
+    aboutme: string;
+    age: number;
+    gender: string;
+}
