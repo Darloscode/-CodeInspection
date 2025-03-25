a3758ee41d6219e9e9e109138a671860d4d2dd7b
MelissaAyllon <mayllon@espol.edu.ec>
2025-03-25 13:12:36 -0500

    ADD send form structure

--- aspy-web/src/components/NewUser.tsx
+++ aspy-web/src/components/NewUser.tsx
@@ -1,3 +1,4 @@
+import { FormProvider, useForm } from "react-hook-form";
 import UserInput from "./UserInput";
 
 function NewUser() {
@@ -7,21 +8,43 @@ function NewUser() {
     { label: "Email", key: "email", type: "email" },
     { label: "Teléfono", key: "phone", type: "number" },
     { label: "Dirección", key: "address", type: "text" },
-    { label: "Numero de Identificación", key: "identification_number", type: "number" },
+    {
+      label: "Numero de Identificación",
+      key: "identification_number",
+      type: "number",
+    },
     { label: "Contraseña", key: "password", type: "password" },
-    { label: "Confirmar Contraseña", key: "confirm_password", type: "password" },
-
+    {
+      label: "Confirmar Contraseña",
+      key: "confirm_password",
+      type: "password",
+    },
   ];
 
+  const methods = useForm();
+  const onSubmit = methods.handleSubmit((data) => {
+    alert(data);
+    console.log(data);
+  });
+
   const list_inputs = input_create_user.map((input) => (
-    <UserInput label={input.label} key={input.key} type={input.type}/>
+    <UserInput label={input.label} key={input.key} type={input.type} />
   ));
-  
+
   return (
-    <div>
-      <h1>New User</h1>
-      <div className="grid grid-cols-2 gap-10">{list_inputs}</div>
-    </div>
+    <FormProvider {...methods}>
+      <form onSubmit={(e) => e.preventDefault()} noValidate>
+        <div>
+          <h1>New User</h1>
+          <div className="grid grid-cols-2 gap-10">{list_inputs}</div>
+        </div>
+        <button
+          onClick={onSubmit}
+        >
+          Submit Form
+        </button>
+      </form>
+    </FormProvider>
   );
 }
 
--- aspy-web/src/components/UserInput.tsx
+++ aspy-web/src/components/UserInput.tsx
@@ -1,9 +1,18 @@
 import TextField from "@mui/material/TextField";
-function UserInput(props: { label: string; key: string, type?: string }) {
+import { useFormContext } from "react-hook-form";
+
+function UserInput(props: { label: string, key: string, type: string }) {
+  const { register } = useFormContext();
+
   return (
     <div className="flex flex-col gap-2">
       <h6>{props.label}</h6>
-      <TextField required id={props.key} type={props.type} variant="outlined" size="small" />
+      <TextField required id={props.label} type={props.type} variant="outlined" size="small" {...register(props.label, {
+          required: {
+            value: true,
+            message: 'required',
+          },
+        })} />
     </div>
   );
 }
