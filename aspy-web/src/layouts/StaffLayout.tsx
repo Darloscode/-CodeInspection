eb09b6b9fcce4f9b7cb4a5a7217e2dfb646a8fe0
Darlos <carfgonz@espol.edu.ec>
2025-03-31 18:31:08 -0500

    UPDATE styles, themes, icons

--- aspy-web/src/layouts/StaffLayout.tsx
+++ aspy-web/src/layouts/StaffLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const StaffLayout = () => (
   <div style={{ display: "flex" }}>
     <SideMenu />
-    <div style={{ flex: 1, marginLeft: 50 }}>
+    <div style={{ flex: 1, marginLeft: 10 }}>
       <Outlet />
     </div>
   </div>
