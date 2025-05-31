4c1dde865bd5d95caec66d5e12a5094f241587cd
MelissaAyllon <mayllon@espol.edu.ec>
2025-05-30 23:52:53 -0500

    UPDATE apiURL in frontend

--- aspy-web/src/API/apiConfig.ts
+++ aspy-web/src/API/apiConfig.ts
@@ -1,4 +1,4 @@
-let apiURL = "http://localhost:8080"; // URL por defecto para local
+let apiURL = "http://localhost:8080/api"; // URL por defecto para local
 const domain = "aspy.ecuador";
 
 if (import.meta.env.VITE_APP_ENV === "production") {
